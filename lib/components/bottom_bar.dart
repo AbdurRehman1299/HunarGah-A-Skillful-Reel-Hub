bccec3bd961f5003e335aa307fbc4bdb45d82099
Abdur Rehman <abdurrehman76001@gmail.com>
2026-03-28 19:15:11 +0500

    create: bottom navigation component

---
+++ lib/components/bottom_bar.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNav extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const CustomBottomNav({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final themeColor = Theme.of(context).primaryColor;
+
+    return Theme(
+      data: Theme.of(context).copyWith(
+        // This removes the splash effect for a cleaner look
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+      ),
+      child: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: onTap,
+        backgroundColor: Colors.transparent,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: themeColor,
+        unselectedItemColor: Colors.white70,
+        selectedLabelStyle: const TextStyle(
+          fontSize: 10,
+          fontWeight: FontWeight.bold,
+        ),
+        unselectedLabelStyle: const TextStyle(fontSize: 10),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: EdgeInsets.only(bottom: 4.0),
+              child: Icon(Icons.home_filled),
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: EdgeInsets.only(bottom: 4.0),
+              child: Icon(Icons.explore_outlined),
+            ),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: EdgeInsets.only(bottom: 4.0),
+              child: Icon(Icons.library_books_outlined),
+            ),
+            label: 'Category',
+          ),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: EdgeInsets.only(bottom: 4.0),
+              child: Icon(Icons.cloud_download_outlined),
+            ),
+            label: 'Downloads',
+          ),
+        ],
+      ),
+    );
+  }
+}
